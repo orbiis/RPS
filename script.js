b0834ca371da35d45b83798216b49b258a118e5c
Boris <krnjic.boris@gmail.com>
2024-05-22 10:09:19 +0100

    Finish playGame functionality

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 // getComputerChoice
+    // math random returns three values
+    // link num  range to string values
+// getHumanChoice
+    // prompt user to choose string value
+    // store string value
+// valueCompare
+    // conditional compare getComputerChoice to getHumanChoice
+    // create relevant returns
+//playRound
+    // combine above
+// playGame
+    // loop playRound until combined score 5
+    // return win/lose message
 
-// math random returns three values
-// link num values to string values
-// test with console.log
 
-let randomValue = Math.random();
-let getComputerChoice
-
-if (randomValue < 0.33) {
-    getComputerChoice = "rock";
-} else if (randomValue > 0.66) {
-    getComputerChoice = "scissors"
-} else {
-    getComputerChoice = "paper"
-}
-
-console.log(getComputerChoice)
-
-// prompt user to choose string value
-// store string value
-// compare human string value to computer string value
-
-let getHumanChoice = prompt("Choose ROCK, PAPER or SCISSORS!").toLowerCase();
-console.log(getHumanChoice);
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound() {
 
-    let humanScore = 0;
-    let computerScore = 0;
+    let randomValue = Math.random();
+    let getComputerChoice
+
+    if (randomValue < 0.33) {
+        getComputerChoice = "rock";
+    } else if (randomValue > 0.66) {
+        getComputerChoice = "scissors"
+    } else {
+        getComputerChoice = "paper"
+    }
+
+    let getHumanChoice = prompt("Choose ROCK, PAPER or SCISSORS!").toLowerCase();
 
     function valueCompare() {
         if (getHumanChoice === "rock" && getComputerChoice === "rock") {
@@ -38,22 +41,22 @@ function playRound() {
             return "It's a draw.";
         } else if (getHumanChoice === "rock" && getComputerChoice === "scissors") {
             ++humanScore;
-            return "You win!";
+            return "You win! Rock beats Scissors.";
         } else if (getHumanChoice === "paper" && getComputerChoice === "rock") {
             ++humanScore;
-            return "You win!";
+            return "You win! Paper beats Rock.";
         } else if (getHumanChoice === "scissors" && getComputerChoice === "paper") {
             ++humanScore;
-            return "You win!";
+            return "You win! Scissors beat paper.";
         } else if (getHumanChoice === "rock" && getComputerChoice === "paper") {
             ++computerScore;
-            return "You lose! :(";
+            return "You lose! Paper beats Rock.";
         } else if (getHumanChoice === "paper" && getComputerChoice === "scissors") {
             ++computerScore;
-            return "You lose! :(";
+            return "You lose! Scissors beat paper.";
         } else if (getHumanChoice === "scissors" && getComputerChoice === "rock") {
             ++computerScore;
-            return "You lose! :(";
+            return "You lose! Rock beats Scissors.";
         } else {
             return "Unsupported value";
         } 
@@ -61,24 +64,20 @@ function playRound() {
 
     console.log(valueCompare())
 
-    console.log(humanScore);
-    console.log(computerScore);
+    console.log("Human: " + humanScore);
+    console.log("Computer: " + computerScore);
 }
 
-playRound();
+function playGame() {
+    
+    while (humanScore + computerScore < 5) {
+        playRound();
+    }
+    if (humanScore > computerScore) {
+        return "You have won the game!"
+    }else {
+        return "You have lost the game!"
+    }
+}
 
-// Declare humanScore
-// Declare computerScore
-// Initialize with 0
-
-
-
-
-
-// Single round logic
-// create function playRound
-// define parameters humanChoice and computerChoice and use as arguments
-
-//  play game
-//      play round x 5
-//          
+playGame();
